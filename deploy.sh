74adcdb202c872788dbaca6f6e1291b6ac7fa293
StevenSong-sTs <songxincheng124@gmail.com>
2024-09-01 21:34:00 -0700

    Add auto deployment script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+# Auto deployment script
+
+# Requirements:
+#  - Copy the code to the Linus server and build and deploy the app
+#  - CI-CD: When code in the main branch is pushed, it will trigger the script and deploy the script. 
